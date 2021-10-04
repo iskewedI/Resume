902a5932f2f3564c62170a0e583ba75c095ce573
iskewedi <jtornello2019@hotmail.com>
2021-10-04 19:29:54 -0300

    Added ProfileImage component

---
+++ src/components/global/ProfileImage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+import Image from '../../misc/img/Me.png';
+
+const useStyles = makeStyles({
+  container: {
+    textAlign: 'center',
+  },
+  image: {
+    maxHeight: '600px',
+    borderRadius: '2%',
+  },
+});
+
+const ProfileImage = ({ styles = {} }) => {
+  const classes = useStyles();
+
+  return <img className={classes.image} style={styles} src={Image} alt='Profile Image' />;
+};
+
+export default ProfileImage;
