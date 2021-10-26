4b2cc8bd2779b3205da3515a3570d2a81295d1d1
iskewedi <jtornello2019@hotmail.com>
2021-10-26 11:28:21 -0300

    Cleaned project | Deleted the Rocket in Space Exploration interest

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,7 +4,6 @@ import { getNextItemInfiniteArray } from '../../logic/Utils';
 import InProgress from '../global/InProgress';
 import ProfileImage from '../global/ProfileImage';
 import Inspectionable from '../lab/Inspection/Inspectionable';
-import Rocket from '../lab/Vehicles/Rocket/Rocket';
 import Moon from '../lab/Moon';
 import { Canvas } from '@react-three/fiber';
 import GeometryLoading from '../global/GeometryLoading';
@@ -97,7 +96,6 @@ const HomePage = () => {
                       <Suspense fallback={<GeometryLoading />}>
                         <pointLight position={[10, 10, 10]} />
                         <Moon position={[0, 5, -25]} />
-                        <Rocket position={[0, -15, -25]} />
                       </Suspense>
                     </Canvas>
                   </div>
--- src/components/scene/MainScene.js
+++ src/components/scene/MainScene.js
@@ -1,25 +1,18 @@
 import { useHelper } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+
 import React, { Suspense, useRef } from 'react';
-import { AxesHelper, Object3D, SpotLightHelper } from 'three';
+import { SpotLightHelper } from 'three';
 import GeometryLoading from '../global/GeometryLoading';
 import Moon from '../lab/Moon';
 import CameraControls from './CameraControls';
 
 const MainScene = () => {
   const spotLightRef = useRef();
-  const lightGroupRef = useRef();
-
-  useFrame(({ clock }) => {
-    // lightGroupRef.current.position.x += 0.1;
-  });
 
   useHelper(spotLightRef, SpotLightHelper, 0.5, 'hotpink');
 
   return (
     <>
-      {/* <ambientLight intensity={0.2} /> */}
-
       <spotLight
         ref={spotLightRef}
         position={[0, -15, 0]}
